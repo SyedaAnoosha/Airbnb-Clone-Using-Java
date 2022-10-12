38eb7b16af338c32bca034e2bfe99a7c9f218ec5
SyedaAnoosha <anushazaidi06@gmail.com>
2022-10-13 02:14:08 +0500

    Threads changed

--- src/Listing.java
+++ src/Listing.java
@@ -3,9 +3,7 @@ import java.io.*;
 class Listing extends Thread{
     String str;
     public synchronized void run() {
-        System.out.println();
         try {
-
             File file = new File("C:\\Users\\anush\\OneDrive\\Desktop\\Listing.txt");
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -18,8 +16,5 @@ class Listing extends Thread{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println();
-        System.out.println();
     }
 }
